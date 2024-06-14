015ae547af2ea3bccbdeef1893c8f3b53a0d635a
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-06-14 16:20:49 +0530

    fixed bugs and added packages

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+dotenv.config();
+const PORTT = process.env.PORT || 5000;
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(5000, () => console.log("Server Running on port 5000"));
+app.use(express.json()); // To parse the body of the request
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORTT, () => {
+  connectToMongoDB();
+  console.log("Server Running on port 5000");
+});
