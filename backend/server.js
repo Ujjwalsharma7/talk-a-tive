4abe14d89e3e19deeac2d26e7b986a651b4e6f41
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-06-21 21:56:24 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,12 +12,20 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 const PORTT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(cookieParser());
 app.use(express.json()); // To parse the body of the request
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORTT, () => {
   connectToMongoDB();
