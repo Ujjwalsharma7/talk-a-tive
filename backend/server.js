d3f7f7bd48d7acbf00f95c33a6009593248bc9e6
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-06-21 20:15:09 +0530

    connected socket to backend

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,10 @@ import authRoutes from "./routes/auth.js";
 import messageRoutes from "./routes/message.js";
 import userRoutes from "./routes/user.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 const PORTT = process.env.PORT || 5000;
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
 app.use(cookieParser());
 app.use(express.json()); // To parse the body of the request
@@ -22,7 +18,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORTT, () => {
+server.listen(PORTT, () => {
   connectToMongoDB();
   console.log("Server Running on port 5000");
 });
