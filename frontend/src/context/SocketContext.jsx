b52489fdd8598c8668d3e97239bef583509d93a2
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-06-21 22:38:09 +0530

    socket url changed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://talkative-9z2d.onrender.com/", {
         query: {
           userId: authUser._id,
         },
